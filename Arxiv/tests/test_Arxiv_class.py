a70b28dd96f780262b17604485c6e112c74643a8
Garrett Williams <garrett.williams@asml.com>
2019-05-29 22:03:25 -0700

    added test

--- Arxiv/tests/test_Arxiv_class.py
+++ Arxiv/tests/test_Arxiv_class.py
@@ -5,6 +5,12 @@ path = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir))
 sys.path.insert(0, path)
 from Arxiv import Arxiv
 
+def test_search_query():
+    print(os.getcwd())
+    authors = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'examples', 'query_information', 'authors.txt') 
+    categories = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'examples', 'query_information', 'categories.txt') 
+    search_query_string = Arxiv.simple_search_query_generator(authors=authors, categories=categories)
+    assert len(search_query_string) > 0
 
 def test_generate_papers():
     arxiv_example = Arxiv(number_of_repeats=2, max_results=5)
@@ -17,3 +23,4 @@ def test_arxiv_papers():
     search_query_string = 'au: Goodfellow OR au: McInnes OR cat: stat.ML'
     arxiv_example.arxiv_papers(search_query_string, store_papers=False)
     assert len(arxiv_example.fav_papers) > 0
+ 
