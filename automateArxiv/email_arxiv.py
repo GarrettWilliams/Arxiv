7d054d6b9ba64a91aa2f6ec78c764513c44cabf7
Garrett Williams <williams.garrettm@gmail.com>
2019-06-02 12:55:26 -0700

    Updated paths. Currently absolute

--- automateArxiv/email_arxiv.py
+++ automateArxiv/email_arxiv.py
@@ -13,29 +13,37 @@ from Arxiv import Arxiv
 import time 
 
 today = str(datetime.datetime.now().date())
+day_of_the_week = datetime.datetime.now().today().weekday()
+valid_days = [0,1,2,3,4]
 root_folder = '/home/pi/Desktop'
 path_to_papers_folder = os.path.join(root_folder, 'Arxiv_papers')
 path_to_individual_date = os.path.join(path_to_papers_folder, today)
 
-if not os.path.exists(path_to_papers_folder):
-    print('Creating papers root directory')
-    os.mkdir(path_to_papers_folder)
-    time.sleep(3)
+if day_of_the_week in valid_days:
+    # Create root paper folder if it doesn't exist.
+    if not os.path.exists(path_to_papers_folder):
+        os.mkdir(path_to_papers_folder)
+        time.sleep(3)
 
-if not os.path.exists(path_to_individual_date):
-    print('Creating papers folder for today')
-    os.mkdir(path_to_individual_date)
+    # Create a folder for a given day.
+    if not os.path.exists(path_to_individual_date):
+        os.mkdir(path_to_individual_date)
 
-arxiv_today = Arxiv()
-print('Starting to look for papers')
-search_string_query  = 'au: Goodfellow OR au: McInnes OR au: Karpathy OR cat: stat.ML'
-todays_papers = arxiv_today.arxiv_papers(search_string_query, store_papers=True, folder_path=path_to_individual_date, date='All')
-print('We found {} papers'.format(len(arxiv_today.fav_papers)))
-recipients = ['williams.garrettm@gmail.com']
-email_subject = 'arxiv papers from {}'.format(today)
+    arxiv_today = Arxiv()
 
-contents = []
-for arxiv_paper in os.listdir(path_to_individual_date):
-    contents.append(arxiv_paper)
+    # Generate search string.
+    authors_path = os.path.join(root_folder, 'Arxiv', 'Arxiv', 'examples', 'query_information', 'authors.txt')
+    categories_path = os.path.join(root_folder, 'Arxiv', 'Arxiv', 'examples', 'query_information', 'categories.txt')
+    search_string_query = arxiv_today.simple_search_query_generator(authors=authors_path, categories=categories_path)
 
-yagmail.SMTP(os.environ.get('automatearxiv_gmail_name'), os.environ.get('automatearxiv_gmail_password')).send(to=recipients, subject=email_subject, contents=contents)
+    todays_papers = arxiv_today.arxiv_papers(search_string_query, store_papers=True, folder_path=path_to_individual_date, date='All')
+
+    # Define list of e-mail recipients and email subject line.
+    recipients = ['williams.garrettm@gmail.com']
+    email_subject = 'arxiv papers from {}'.format(today)
+
+    contents = []
+    for arxiv_paper in os.listdir(path_to_individual_date):
+        contents.append(os.path.join(path_to_individual_date, arxiv_paper))
+
+    yagmail.SMTP(os.environ.get('automatearxiv_gmail_name'), os.environ.get('automatearxiv_gmail_password')).send(to=recipients, subject=email_subject, contents=contents)
